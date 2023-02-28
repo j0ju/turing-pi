511bafc9c9e0ae5ac77f16a04a5a35b9a94ae15d
Daniel Kukieła <daniel@kukiela.pl>
2023-02-28 17:26:36 +0100

    Cleanup

--- mkfw.sh
+++ mkfw.sh
@@ -4,16 +4,6 @@ date=`date +%F`
 
 version="0.1.0-wip"
 
-if [ 1 != $# ]; then
-	echo please input version
-fi
-echo $1 |grep "^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$" > /dev/null
-if [ $? = 1 ];then
-	echo version 0~999 and must 3 version eg:1.1.1
-	echo ./mkfw.sh "<version>"
-	exit 1
-fi
-
 if [ ! -d "build/${date}" ];then
     echo "mkdir build/${date}"
     mkdir -p "build/${date}"
@@ -35,10 +25,4 @@ cd -
 cp -rf ./buildroot/output/images/turingpi_.swu ./build/${date}/turingpi-${version}.swu
 
 echo "build turing pi firmware over"
-if [ ! -f "build/tpi/linux/tpi" ];then
-	mkdir -p build/tpi/linux
-	echo "build tpi cli"
-	gcc app/tpi/tpi.c -o build/tpi/linux/tpi
-	echo "build tpi cli over"
-fi
 
