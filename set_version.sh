61d60f9ab10634e94bbf6c093eae252287ab9288
Daniel Kukieła <daniel@kukiela.pl>
2023-02-28 20:10:48 +0100

    Fixed an issue where the version change breaks the script

--- set_version.sh
+++ set_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i "s/version: .*$/version: $1/" app/board/rootfs_overlay/etc/init.d/s99hello.sh
+sed -i "s/version: .*$/version: $1\"/" app/board/rootfs_overlay/etc/init.d/s99hello.sh
 sed -i "s/version=.*$/version=\"$1\"/" mkfw.sh
 sed -i "s/CONTAINER_VER=.*$/CONTAINER_VER=\"$1\"/" bmc4tpi/swupdate/genSWU.sh
