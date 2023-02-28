1015f17c327dc96f84e6e693256f2f8c28c51424
Daniel Kukieła <daniel@kukiela.pl>
2023-02-28 20:11:58 +0100

    Fixed broken echo

--- app/board/rootfs_overlay/etc/init.d/S99hello.sh
+++ app/board/rootfs_overlay/etc/init.d/S99hello.sh
@@ -5,7 +5,7 @@ echo "|_   _| | | |  _ \|_ _| \ | |/ ___| |  _ \|_ _| |_  \ "
 echo "  | | | | | | |_) || ||  \| | |  _  | |_) || |    ) | "
 echo "  | | | |_| |  _ < | || |\  | |_| | |  _ / | |   / /  "
 echo "  |_|  \___/|_| \_|___|_| \_|\____| |_|   |___| |___| "
-echo "Community Updates by DhanOS, version: 0.1.0-wip
+echo "Community Updates by DhanOS, version: 0.1.0-wip"
 
 bmc &
 mount /dev/mmcblk0p1 /mnt/sdcard/
