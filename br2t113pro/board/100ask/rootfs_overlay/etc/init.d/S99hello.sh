6db74211a9ba41bb198ce4c7ce2cfce429c76a3f
Daniel Kukieła <daniel@kukiela.pl>
2023-03-05 19:18:56 +0100

    Moved the welcome message below the initialization scripts

--- br2t113pro/board/100ask/rootfs_overlay/etc/init.d/S99hello.sh
+++ br2t113pro/board/100ask/rootfs_overlay/etc/init.d/S99hello.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 
+bmc &
+mount /dev/mmcblk0p1 /mnt/sdcard/
+echo 3 4 1 7 > /proc/sys/kernel/printk
+#/etc/test_ping.sh &
+
 echo " _____ _   _ ____  ___ _   _  ____   ____  ___   ___  "
 echo "|_   _| | | |  _ \|_ _| \ | |/ ___| |  _ \|_ _| |_  \ "
 echo "  | | | | | | |_) || ||  \| | |  _  | |_) || |    ) | "
 echo "  | | | |_| |  _ < | || |\  | |_| | |  _ / | |   / /  "
 echo "  |_|  \___/|_| \_|___|_| \_|\____| |_|   |___| |___| "
 echo "Community Updates by DhanOS, version: 0.2.0-wip"
-
-bmc &
-mount /dev/mmcblk0p1 /mnt/sdcard/
-echo 3 4 1 7 > /proc/sys/kernel/printk
-/etc/test_ping.sh &
+echo ""
