c73c802a3b232b2669bb11db45a6062d1a717895
Daniel Kukieła <daniel@kukiela.pl>
2023-03-09 16:26:46 +0100

    Updated firmware and board names

--- bmc4tpi/swupdate/genSWU.sh
+++ bmc4tpi/swupdate/genSWU.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CONTAINER_VER="0.2.0-wip"
-PRODUCT_NAME="turing_pi2_ce"
+PRODUCT_NAME="turing_pi_2_ce"
 FILES="sw-description rootfs.ubifs env0.fex env1.fex"
 for i in $FILES;do
         echo $i;done | cpio -ov -H crc >  ${PRODUCT_NAME}-${CONTAINER_VER}.swu
