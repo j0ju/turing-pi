59cdd1f78ddeff79ac90247cbe440dcd8eb9bb99
Joerg Jungermann <abra@0x2f.org>
2023-04-27 23:20:42 +0200

    post-build-script: remove and rescue kernel modules, we have only limited space in flash

--- br2t113pro/board/100ask/t113-pro/post-build-spinand.sh
+++ br2t113pro/board/100ask/t113-pro/post-build-spinand.sh
@@ -1,6 +1,33 @@
 #!/bin/sh
 BOARD_DIR="$(dirname $0)"
 
+MOD_REMOVE=
+MOD_REMOVE="$MOD_REMOVE ./fs/cifs"
+MOD_REMOVE="$MOD_REMOVE ./fs/btrfs"
+MOD_REMOVE="$MOD_REMOVE ./fs/fuse"
+MOD_REMOVE="$MOD_REMOVE ./fs/overlayfs"
+MOD_REMOVE="$MOD_REMOVE ./fs/autofs"
+MOD_REMOVE="$MOD_REMOVE ./mm"
+MOD_REMOVE="$MOD_REMOVE ./net"
+MOD_REMOVE="$MOD_REMOVE ./drivers/net"
+
+
+ROOT_DIR="$BASE_DIR/target"
+MODULES_DIR="$BASE_DIR/target_modules"
+
+rm -rf "$MODULES_DIR"
+mkdir "$MODULES_DIR"
+
+# there should only be one directory for modules (only one kernel in image)
+cd "$ROOT_DIR/lib/modules"/[0-9]*/kernel
+SRC_REL="${PWD#$ROOT_DIR}"
+
+for d in $MOD_REMOVE; do
+  [ -d "$d" ] || continue
+  dst="$MODULES_DIR/$SRC_REL/$d"
+  mkdir -p "${dst%/*}"
+  mv -fv "$d" "${dst%/*}"
+done
 
 # For debug
 echo "Target binary dir $BOARD_DIR"
@@ -16,7 +43,6 @@ cp $BOARD_DIR/../pack_img/* -rfvd  $BINARIES_DIR
 
 #Copy tina pack tools
 cp $BOARD_DIR/../tina-pack-tools/* -rfvd  $BINARIES_DIR
-
 
 #cd buildroot/output/images/
 cd $BINARIES_DIR
