1ad5d0f7792b53629ab66937d44924119a5a732d
Joerg Jungermann <abra@0x2f.org>
2023-04-28 19:21:33 +0200

    mkfw.sh: rebuild also linux on build in case of kernel config changes

--- mkfw.sh
+++ mkfw.sh
@@ -31,6 +31,7 @@ echo "Date: ${date}"
 
 echo "build fw"
 make -C buildroot bmc-rebuild V=1
+make -C buildroot linux-rebuild V=1
 make -C buildroot V=1
 ( set -x
   cp -rf buildroot/output/images/buildroot_linux_nand_uart3.img ./build/${date}/turingpi-${version}.img
@@ -42,11 +43,9 @@ cd -
 
 mv -f ./buildroot/output/images/turingpi_.swu ./build/${date}/turingpi-${version}.swu
 
-echo "build turing pi firmware over"
+echo "build turing pi firmware CLI"
 if [ ! -f "build/tpi/linux/tpi" ];then
 	mkdir -p build/tpi/linux
-	echo "build tpi cli"
 	gcc app/tpi/tpi.c -o build/tpi/linux/tpi
-	echo "build tpi cli over"
 fi
 
